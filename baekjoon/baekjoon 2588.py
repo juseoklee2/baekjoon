3866241c8c2e87e070a45680b82e506a70e73c2c
juseoklee2 <juseok2happy@naver.com>
2025-02-02 16:41:11 +0900

    baekjoon 2588.py

--- baekjoon/baekjoon 2588.py
+++ baekjoon/baekjoon 2588.py
@@ -1,3 +1,23 @@
+'''
+Problem:
+
+Multiplying two three-digit numbers involves the following steps:
+
+When three-digit natural numbers are given for positions (1) and (2), the values for positions (3), (4), (5), and (6) need to be calculated.
+
+Input:
+
+The first line contains the three-digit natural number to be placed in position (1).
+The second line contains the three-digit natural number to be placed in position (2).
+Output:
+
+Output the values for positions (3), (4), (5), and (6) in order:
+(3) is the product of the one's place of the first number and the one's place of the second number,
+(4) is the product of the ten's place of the first number and the one's place of the second number,
+(5) is the product of the hundred's place of the first number and the one's place of the second number,
+(6) is the final multiplication result of both numbers.
+'''
+
 #First, receive two three-digit number.
 one = int(input())
 two = int(input())
