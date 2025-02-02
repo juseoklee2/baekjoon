a4ceabf230fc473dc37738492a160c789c043f5b
juseoklee2 <juseok2happy@naver.com>
2025-02-02 16:43:56 +0900

    baekjoon 10171.py

--- baekjoon/baekjoon 10171.py
+++ baekjoon/baekjoon 10171.py
@@ -1,3 +1,11 @@
+'''
+Problem: Print a cat as shown in the example below.
+
+Input: None.
+
+Output: Print a cat.
+'''
+
 #To print \(backslash), it requires \\(Double backslash).
 print("\\    /\\")
 print(" )  ( ')")
